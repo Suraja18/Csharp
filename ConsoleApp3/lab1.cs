1b8c657ea357839e7b1b39f49e99f1f571cd4f98
Suraj Adhikari <sssadhikari222@gmail.com>
2023-05-24 07:49:11 +0545

    First

---
+++ ConsoleApp3/lab1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class lab1
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.Clear();
+            Console.WriteLine("Suraj");
+        } 
+    }
+}
