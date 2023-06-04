9a7b39fd7c0a28294b10eec212d838a28bb26967
Suraj Adhikari <sssadhikari222@gmail.com>
2023-06-04 07:06:22 +0545

    2nd Class

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace InheritanceLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messages.Greet();
+            LabObject newLab = new LabObject(1,"Suraj");
+            newLab.ShowDetails();
+            LabObject newLab2 = new LabObject(2, "Samir");
+            newLab2.ShowDetails();
+            LabObject newLab3 = new LabObject();
+            newLab3.ShowDetails();
+        }
+    }
+    class LabObject
+    {
+        private int id = 14;
+        public string name = "Suraj";
+        //Create Default value if no argument were pass on LabObject
+        public LabObject()
+        {
+            this.id = 14;
+            this.name = "Arjun";
+        }
+        //Constructor Create
+        public LabObject(int id, string name) 
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Name of Student:" + name + " ID:" + id);
+        }
+        public void SetID(int id) 
+        {
+            this.id = id;
+
+        }
+    }
+}
--- ConsoleApp3/lab1.cs
+++ ConsoleApp3/lab1.cs
@@ -8,26 +8,28 @@ namespace ConsoleApp3
 {
     public class lab1
     {
-        static void Main(string[] args)
-        {
-            if (args is null)
-            {
-                throw new ArgumentNullException(nameof(args));
-            }
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Black;
+        //static void Main(string[] args)
+        //{
+        //    if (args is null)
+        //    {
+        //        throw new ArgumentNullException(nameof(args));
+        //    }
+        //    Console.BackgroundColor = ConsoleColor.White;
+        //    Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.Clear();
-            Console.WriteLine("Hello this is CSharp Class");
-            //WriteLine Print the line in nextline.
-            Console.Write("Lets Learn Something New.");
-            Console.Write("Lets Learn Something old");
-            Console.WriteLine();
+        //    Console.Clear();
+        //    Console.WriteLine("Hello this is CSharp Class");
+        //    //WriteLine Print the line in nextline.
+        //    Console.Write("Lets Learn Something New.");
+        //    Console.Write("Lets Learn Something old");
+        //    Console.WriteLine();
 
-            //Read Line input by user
-            Console.Write("Write Something: ");
-            Console.WriteLine(Console.ReadLine());
-      
-        } 
+        //    //Read Line input by user
+        //    Console.Write("Write Something: ");
+        //    Console.WriteLine(Console.ReadLine());
+        //    Console.Write("Write Something to find its ASCII: ");
+        //    Console.WriteLine(Console.Read());
+
+        //} 
     }
 }
